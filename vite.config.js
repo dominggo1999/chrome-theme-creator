0bc38769ab53ed822b4ab9744294690915a69588
unknown <arnolddominggo19@gmail.com>
2022-04-22 20:08:38 +0700

    upgrade vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-// vite.config.json
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import macrosPlugin from 'vite-plugin-babel-macros';
+import react from '@vitejs/plugin-react';
 import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), macrosPlugin(), wasmPack([], ['photon-web'])],
-  define: {
-    'process.platform': JSON.stringify('win32'),
-    'process.env': {},
-  },
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },
+    }),
+    wasmPack([], ['photon-web']),
+  ],
 });
